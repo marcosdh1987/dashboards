4844fc7c7c9335c120f5acadd7f110af2cd58a3a
marcosdh1987 <marcos.esteban.soto@gmail.com>
2021-06-04 14:14:08 -0300

    realtime and home improved views

--- src/dash/urls.py
+++ src/dash/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import comparisonView,HomeView
+from .views import comparisonView,HomeView, dashboardView,realtimeView
 
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('comparison/', comparisonView.as_view(), name='comparison'),
-
+    path('dashboard/', dashboardView.as_view(), name='dashboard'),
+    path('realtime/', realtimeView.as_view(), name='realtime'),
 ]
--- src/src/urls.py
+++ src/src/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from dash.views import HomeView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
